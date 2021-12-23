129208365b626427439d9b557afa83ebf92a7059
SAimanZ <aimanzaheer66@gmail.com>
2021-12-23 17:38:48 +0500

    ready

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,30 +5,49 @@ export const Main = () => {
         <div>
           
 <div class="container-fluid">
-  <div class="row">
-          <div class="col-4 bg-success p-4 BACK"><h2>The purpose of our lives is to be happy</h2></div>
+  <div class="row main ">
+          <div class="col-4  p-4 BACK"><h2>The purpose of our lives is to be happy</h2><p className='font'>😊</p></div>
     <div class="col-8 p-0">
-    <div className=' container-fluid bg-warning p-4 BACK1 '> <h1>Life is what happens when you’re busy making other plans.</h1></div> 
-      <div class="row m-0">
-        <div class="col-6 bg-danger p-4 BACK2"><h3>Get busy living or get busy dying.</h3></div>
-        <div class="col-6 bg-secondary p-4 BACK3">.col-6</div>
+    <div className=' container-fluid  p-4 BACK1 '> <h1>Life is what happens when you’re busy making other plans.</h1></div> 
+      <div class="row m-0 rowheight">
+        <div class="col-6  p-4 BACK2"><h3 className='style'>Get busy living or get busy dying.</h3></div>
+        <div class="col-6  p-4 BACK3"><h3 className='style'>Get busy living or get busy dying</h3></div>
       </ div>
       </div></div></div> 
       <div class="m-1 line" > </div>
       
-      <div class="bg-primary container p-5"> container 2</div>
+    
+      <div class="container  p-3 mt-3">
+   <ul class="nav nav-tabs">
+    <li class="nav-item">
+    <a class="nav-link" href="#">Latest</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Topviews</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Update</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Todo List</a>
+    </li>
+  </ul>
+</div>
+      
       <div class="m-1 line" > </div>
       <div class=" container p-5">
       <div class="row">
-          <div class="col-4 bg-success p-4">.col-4</div>
+          <div class="col-4 BACK5 p-4"><h1 className="style">Live in the leading—the spaces in between the rules.</h1></div>
     <div class="col-8 p-0">
    <div class="row m-1">
-        <div class="col-6"><div className='bg-dark p-4'>.col-6</div></div>
-        <div class="col-6 "><div className='bg-danger p-4'>.col-6</div></div>
+        <div class="col-6"><div className='BACK6 p-4'>
+          <h1 className="style">Have no fear of perfection—you'll never reach it.</h1></div></div>
+        <div class="col-6 "><div className='BACK7 p-4'> 
+           <h1 className="style">Have no fear of perfection—you'll never reach it.</h1></div></div>
       </ div>
    <div class="row m-1">
-        <div class="col-6 "><div className='bg-info p-4'> .col-6</div></div>
-        <div class="col-6 "><div className='bg-success p-4'>.col-6</div></div>
+        <div class="col-6 "><div className='bg-info p-4'>     <h1 className="style">Digital design is like painting, except the paint never dries.</h1></div></div>
+        <div class="col-6 "><div className='bg-success p-4'>    <h1 className="style">It's easier to ask forgiveness than it is to get permission.</h1></div></div>
       </ div>
       </div></div>
       </div>
@@ -36,8 +55,8 @@ export const Main = () => {
       <div class="row m-1">
         <div class="col-8 ">
         <div class="row m-1">
-        <div class="col-6  "><div className='bg-info p-5 height'> .col-6</div></div>
-        <div class="col-6  "><div className='bg-success p-5 height'>.col-6</div></div>
+        <div class="col-6  "><div className='bg-info p-5 height'> <h1 className="style">Creativity is nothing but a mind set free.</h1></div></div>
+        <div class="col-6  "><div className='bg-success p-5 height'><h1 className="style">It's easier to ask forgiveness than it is to get permission</h1></div></div>
       </ div>
         </div>
         <div class="col-4 ">
